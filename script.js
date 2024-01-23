e5e6aec6ed685047a51ebb03b6673a07a2b5cc6a
Manvith Kumar shetty <121666002+manvithshetty507@users.noreply.github.com>
2024-01-23 19:59:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const extendHex = (shortHex) => {
   // write your code here
-	let arr = Array.from(shortHex.substring(1))
+	let arr = (shortHex[0] === '#') ? Array.from(shortHex.substring(1)) : Array.from(shortHex)
 	const ans = arr.map((ele,i) => ele + ele)
 	return '#' + ans.join('')
 };
